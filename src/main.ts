81ee8e072a4dc84138a2377095b81ce4b6fe5bf0
vkozodoy <vladislav.kozodoi@gmail.com>
2025-02-11 13:31:06 +0300

    updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-// Определение маршрутов
 const routes = [
     {
-      path: '/',
+      path: '/home',
       component: () => import('./components/HelloWorld.vue'),
     },
   ];
   
-  // Создание маршрутизатора
   const router = createRouter({
     history: createWebHistory(),
     routes,
